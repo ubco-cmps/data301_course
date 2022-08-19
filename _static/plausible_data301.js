1bf45cc14f75ceeaea924e8b87bf1a4ded26872c
Firas Moosvi <firas@moosvi.com>
2022-08-19 10:27:08 -0700

    update PA

--- _static/plausible_data301.js
+++ _static/plausible_data301.js
@@ -1,7 +1,7 @@
 var script = document.createElement("script");
 script.defer = true;
 script.src = "https://plausible.io/js/script.js";
-script.dataset.domain = "firas.moosvi.com/courses/data301";
+script.dataset.domain = "data301";
 
 // optional if using proxy
 // script.dataset.api = 'https://yourproxy.com/api/event';
